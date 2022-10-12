be7d3d1222165d32f642a1dbd0bb5e38520cb592
Anh Nguyen <anguyen@Nguyen-MacBook-Pro.local>
2022-10-12 14:25:37 +1100

    added subtraction route

--- Program.cs
+++ Program.cs
@@ -9,10 +9,16 @@ app.MapGet("/add", () => AddTwoIntegers(9, 8));
 // parameter routing
 app.MapGet("/add/{input1}/{input2}", (int input1, int input2) => AddTwoIntegers(input1, input2));
 
+app.MapGet("/sub/{num1}/{num2}", (int num1, int num2) => SubtractTwoNumbers(num1, num2));
+
 app.Run();
 
 // function to add two integers together
 int AddTwoIntegers(int num1, int num2) {
     int ans = num1 + num2;
     return ans;
+}
+
+int SubtractTwoNumbers(int num1, int num2) {
+    return num1 - num2;
 }
